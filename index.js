f31022fec63cb41d55bd9f68fe0728fb70a6ea4b
Neuton Foo <neuton.foo@gmail.com>
2021-04-21 17:37:28 -0700

    Initial commit

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/text", (req, res) => {
+  const message = req.body.message;
+
+  console.log(`The message is ${message}.`);
+
+  res.send("Message!");
 });
 
 app.listen(port, () => {
